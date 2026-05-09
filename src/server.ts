ba41009a0a0eba4f090315fdc52bbcad031bdd2d
Huynh Nguyen Phuong Duy <huynhduylp@gmail.com>
2026-05-09 19:55:30 +0700

    feat: initialize Node.js API project with Express, TypeScript, and ESLint

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
