36648afe946d3097d35366169d45e95f70d344e0
Huynh Nguyen Phuong Duy <huynhduylp@gmail.com>
2026-05-15 17:05:30 +0700

    feat: add searchUserByUsername endpoint to retrieve user details by username

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import User from '~/models/Users.js';
 
 export const authMe = async (req: Request, res: Response) => {
   try {
@@ -9,6 +10,22 @@ export const authMe = async (req: Request, res: Response) => {
     });
   } catch (error) {
     console.log('Lỗi khi goi authMe', error);
+    return res.status(500).json({ message: 'Lỗi hệ thống, vui lòng thử lại sau' });
+  }
+};
+
+export const searchUserByUsername = async (req: Request, res: Response) => {
+  try {
+    const { username } = req.query;
+
+    if (!username || (username as string).trim() === '') {
+      return res.status(400).json({ message: 'Cần cung cấp username trong query ' });
+    }
+    const user = await User.findOne({ username: username as string }).select('_id username displayName avatarUrl');
+
+    return res.status(200).json({ user });
+  } catch (error) {
+    console.log('Lỗi xảy ra khi gọi searchUserByUsername', error);
     return res.status(500).json({ message: 'Lỗi hệ thống, vui lòng thử lại sau' });
   }
 };
